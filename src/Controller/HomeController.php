f2dfcad59e0d116ab618b3156d251a9ae03bd50a
Yacine RAFES <yacine.rafes@gmail.com>
2025-08-04 14:33:28 +0200

    Ajout la récupération des catégories dans la BDD

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -2,15 +2,20 @@
 
 namespace App\Controller;
 
+use App\Repository\CategoryRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
+use Symfony\Component\HttpFoundation\Request;
 
 final class HomeController extends AbstractController
 {
     #[Route('/', name: 'home')]
-    public function index(): Response
+    public function index(Request $request, CategoryRepository $category): Response
     {
-        return $this->render('home/index.html.twig');
+        $categories = $category->findAll();
+        return $this->render('home/index.html.twig', [
+                'categories' => $categories,
+            ]);
     }
 }
