4dda800569b299407a0543511666ad52d3908701
Yacine RAFES <yacine.rafes@gmail.com>
2025-08-06 20:00:06 +0200

    Refactor le contrôleur ProductController : mise à jour de la méthode index pour récupérer un produit et gestion des exceptions, ajout du template product.html.twig

--- src/Controller/ProductController.php
+++ src/Controller/ProductController.php
@@ -2,17 +2,30 @@
 
 namespace App\Controller;
 
+use App\Repository\ProductRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
+use Symfony\Component\HttpFoundation\Request;
 
 final class ProductController extends AbstractController
 {
     #[Route('/product/{id}', name: 'product')]
-    public function index(): Response
+    public function index(
+        int $id,
+        ProductRepository $productRepository
+    ): Response
     {
-        return $this->render('product/index.html.twig', [
-            'controller_name' => 'ProductController',
+        // Je récupère le produit depuis le ProductRepository
+        $product = $productRepository->find($id);
+
+        // Si le produit n'existe pas, on lance une exception
+        if (!$product) {
+            throw $this->createNotFoundException('Produit non trouvé');
+        }
+
+        return $this->render('product/product.html.twig', [
+            'product' => $product
         ]);
     }
 }
